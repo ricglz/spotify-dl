292d1b00fde9e7ce2ef83d7e23eccee8e20a7d5f
Ricardo J González C <rjgcastillo@hotmail.com>
2021-02-28 14:56:27 -0600

    Remove useless code and fix indentation

--- spotify-dl.py
+++ spotify-dl.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python
 
-#from googleapiclient.discovery import build
-#from apiclient.errors import HttpError
-#from oauth2client.tools import argparser
-
 import urllib
 import urllib2
 from bs4 import BeautifulSoup
 import argparse
-#import spotify
 import json
 from StringIO import StringIO
 import subprocess
@@ -29,47 +24,6 @@ OK      =  GREEN + "[+] " + DEFAULT
 #=======================
 CLIENT_ID=""
 CALL_BACK_URL=""
-
-# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
-# tab of
-#   https://cloud.google.com/console
-# Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "REPLACE_ME"
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
-
-def youtube_search(options):
-  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-    developerKey=DEVELOPER_KEY)
-
-  # Call the search.list method to retrieve results matching the specified
-  # query term.
-  search_response = youtube.search().list(
-    q=options.q,
-    part="id,snippet",
-    maxResults=options.max_results
-  ).execute()
-
-  videos = []
-  channels = []
-  playlists = []
-
-  # Add each result to the appropriate list, and then display the lists of
-  # matching videos, channels, and playlists.
-  for search_result in search_response.get("items", []):
-    if search_result["id"]["kind"] == "youtube#video":
-      videos.append("%s (%s)" % (search_result["snippet"]["title"],
-                                 search_result["id"]["videoId"]))
-    elif search_result["id"]["kind"] == "youtube#channel":
-      channels.append("%s (%s)" % (search_result["snippet"]["title"],
-                                   search_result["id"]["channelId"]))
-    elif search_result["id"]["kind"] == "youtube#playlist":
-      playlists.append("%s (%s)" % (search_result["snippet"]["title"],
-                                    search_result["id"]["playlistId"]))
-
-  print "Videos:\n", "\n".join(videos), "\n"
-  print "Channels:\n", "\n".join(channels), "\n"
-  print "Playlists:\n", "\n".join(playlists), "\n"
 
 def searchYoutube(trackname):
     textToSearch = trackname
@@ -120,44 +74,40 @@ def downloadYoutube(link):
     print OK + "Song Downloaded"
 
 def header():
-	""" header informations """
-	print RED + "@ spotify-dl.py version 0.0.1"
-	print YELLOW + "@ author : Naper"
-	print BLUE + "@ Designed for OSx/linux"
-	print "" + DEFAULT
-
+    """ header informations """
+    print RED + "@ spotify-dl.py version 0.0.1"
+    print YELLOW + "@ author : Naper"
+    print BLUE + "@ Designed for OSx/linux"
+    print "" + DEFAULT
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description='spotify-dl allows you to download your spotify songs')
-  parser.add_argument('--verbose',
-    action='store_true',
-    help='verbose flag' )
-  parser.add_argument('--dl', nargs=1, help="set the download methode")
-  parser.add_argument('--user', nargs=1, help="set the spotify login")
-  parser.add_argument('--password', nargs=1, help="set the spotify password")
-  parser.add_argument('--traceback', action='store_true', help="enable traceback")
-  parser.add_argument('--gen_url', action='store_true', help="generate url for getting access_token")
-  parser.add_argument('--track', nargs=1, help="spotify track id")
-  parser.add_argument('--access_token', nargs=1, help="set the access_token")
-  parser.add_argument('-m', nargs=1, help="set a methode")
+    parser = argparse.ArgumentParser(description='spotify-dl allows you to download your spotify songs')
+    parser.add_argument('--verbose',
+              action='store_true',
+              help='verbose flag' )
+    parser.add_argument('--dl', nargs=1, help="set the download methode")
+    parser.add_argument('--user', nargs=1, help="set the spotify login")
+    parser.add_argument('--password', nargs=1, help="set the spotify password")
+    parser.add_argument('--traceback', action='store_true', help="enable traceback")
+    parser.add_argument('--gen_url', action='store_true', help="generate url for getting access_token")
+    parser.add_argument('--track', nargs=1, help="spotify track id")
+    parser.add_argument('--access_token', nargs=1, help="set the access_token")
+    parser.add_argument('-m', nargs=1, help="set a methode")
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  try:
-      header();
-      if args.gen_url:
-          genUrl()
-      else:
-          if args.dl and args.access_token and args.dl[0] == 'youtube':
-              if args.track:
-                  #genUrl()
-                  #getAccessToken()
-                  name = getTrackName(args.track[0], args.access_token[0])
-              link = searchYoutube(name)
-              downloadYoutube(link)
-          else :
-              print ERROR + "use --help for help"
-  except Exception, err:
-    print ERROR + "An HTTP error occurred\n"
-    if args.traceback:
-    	traceback.print_exc()
+    try:
+        header();
+        if args.gen_url:
+            genUrl()
+        else:
+            if args.dl and args.access_token and args.dl[0] == 'youtube' and args.track:
+                name = getTrackName(args.track[0], args.access_token[0])
+                link = searchYoutube(name)
+                downloadYoutube(link)
+            else:
+                print ERROR + "use --help for help"
+    except Exception, err:
+        print ERROR + "An HTTP error occurred\n"
+        if args.traceback:
+            traceback.print_exc()
